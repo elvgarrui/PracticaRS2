1e36398b111451bbf9b3ffe3d9cd4df651beff8f
elvgarrui <elviragruiz@gmail.com>
2017-12-21 14:10:48 +0100

    Settings y urls

--- PracticaRS2/settings.py
+++ PracticaRS2/settings.py
@@ -110,6 +110,7 @@ ROOT_URLCONF = 'PracticaRS2.urls'
 WSGI_APPLICATION = 'PracticaRS2.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(RUTA_PROYECTO, 'plantillas'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- PracticaRS2/urls.py
+++ PracticaRS2/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
+admin.autodiscover()
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'PracticaRS2.views.home', name='home'),
-    # url(r'^PracticaRS2/', include('PracticaRS2.foo.urls')),
+    url(r'^$', 'PracticaRS2.views.inicial', name='inicial'),
+    url(r'^tusArtistas/', include('PracticaRS2.views.tusartistas')),
+    url(r'^topEscuchado/', include('PracticaRS2.views.topartistas')),
+    url(r'^descubre/', include('PracticaRS2.views.descubre')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
