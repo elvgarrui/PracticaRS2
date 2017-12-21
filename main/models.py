ef9d1802ab9e73b86dafb4f1430f6aa3883268f5
caolin333 <raulrp1996@gmail.com>
2017-12-21 14:16:54 +0100

    Añade modelos artista y etiquieta

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Artista(models.Model):
+    idArtista = models.IntegerField(null=True, blank=True)
+    nombre = models.CharField(null=True, blank=True)
+    url = models.URLField(validators=[URLValidator()], null=True, blank=True)
+    pictureUrl = models.URLField(validators=[URLValidator()], null=True, blank=True)
+    def __unicode__(self):
+        return self.nombre
+    
+class Etiqueta():
+    idTag = models.IntegerField(null=True, blank=True)
+    tagValue = models.CharField(null=True, blank=True)
+    def __unicode__(self):
+        return self.tagValue
+    
